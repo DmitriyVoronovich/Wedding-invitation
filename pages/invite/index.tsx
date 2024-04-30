28174f3e0e7849e27033aeb85353ad88280f8ed2
Pavel_Varanovich <pavel_varanovich@epam.com>
2024-04-30 10:08:07 +0200

    Change location for the style import

--- pages/invite/index.tsx
+++ pages/invite/index.tsx
@@ -1,10 +1,5 @@
 import s from './index.module.scss';
-import HeaderMenu from "@/app/components/first/Header/Header";
-import MainSection from "@/app/components/first/MainSection/MainSection";
-import InvitationSection from "@/app/components/first/InvitationSection/InvitationSection";
-import ContactsSection from "@/app/components/first/ContactsSection/ContactsSection";
-import ScheduleSection from "@/app/components/first/ScheduleSection/ScheduleSection";
-import { Layout } from 'antd';
+import {Layout} from 'antd';
 import {HeaderComponent} from "@/app/components/second/header/header-component";
 import '../../app/components/second/style_ant.css'
 import {MainSectionComponent} from "app/components/second/content/main-component";
@@ -13,23 +8,49 @@ import InviteComponent from "@/app/components/second/content/invite-component";
 import {SectionOneComponent} from "@/app/components/second/content/section-one";
 import {SectionTwoComponent} from "@/app/components/second/content/section_two";
 import {FooterComponent} from "@/app/components/second/content/footer-component";
-const { Header, Footer, Content } = Layout;
 import './style.css'
 
+const {Header, Footer, Content} = Layout;
+
+
+const PageLayout = ({children}: any) => {
+    return (
+        <html lang="en">
+        <head>
+            <title>Приглашение</title>
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com"/>
+            <link
+                href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+                rel="stylesheet"/>
+            <style jsx global>{`
+                body {
+                    margin: 0;
+                }
+            `}</style>
+        </head>
+        <body>{children}</body>
+        </html>
+    )
+}
 
 export default function Home() {
     return (
-            <Layout className={s.layout_style}>
-                <Header className={s.header_style}><HeaderComponent/></Header>
-                <Content className={s.content_style}>
-                    <MainSectionComponent/>
-                    <InviteComponent/>
-                    <SectionOneComponent/>
-                    <ScheduleSectionComponent/>
-                    <SectionTwoComponent/>
-                </Content>
-                <Footer className={s.footer_style}><FooterComponent/></Footer>
-            </Layout>
+        <>
+            <PageLayout>
+                <Layout className={s.layout_style}>
+                    <Header className={s.header_style}><HeaderComponent/></Header>
+                    <Content className={s.content_style}>
+                        <MainSectionComponent/>
+                        <InviteComponent/>
+                        <SectionOneComponent/>
+                        <ScheduleSectionComponent/>
+                        <SectionTwoComponent/>
+                    </Content>
+                    <Footer className={s.footer_style}><FooterComponent/></Footer>
+                </Layout>
+            </PageLayout>
+        </>
         // <main className={s.main}>
         //     <HeaderMenu/>
         //     <MainSection/>
