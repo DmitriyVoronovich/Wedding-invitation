1bbe330e082560fa71c9d4f31442a4e7028955f9
Dmitriy Vorornovich <dmitriy.vr20@gmail.com>
2024-05-06 21:08:39 +0300

    feat: added burger menu

--- app/components/second/header/header-component/index.tsx
+++ app/components/second/header/header-component/index.tsx
@@ -1,10 +1,12 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {MENU_HEADER_LIST} from "@/app/constant/constant";
 import s from './header-component.module.scss'
 import Link from "next/link";
+import {CloseOutlined} from "@ant-design/icons";
 
 export const HeaderComponent = () => {
     const [width, setWidth] = React.useState(0);
+    const [open, setOpen] = useState(false);
 
     useEffect(() => {
         const handleWindowResize = () => setWidth(window.innerWidth)
@@ -13,30 +15,51 @@ export const HeaderComponent = () => {
         return () => window.removeEventListener('resize', handleWindowResize);
     });
 
+    const onMenuToggle = () => {
+        setOpen(!open)
+    }
+
     return (
         <section className={s.header_container}>
             <div className={s.container}>
-                <Link href={`/invite`} className={s.menu_link}>
-                    <div className={s.header_logo}>А&П</div>
-                </Link>
-                {width < 768
-                    ? <div className={s.burger_menu}>
-                        <span className={s.burger_item}></span>
-                        <span className={s.burger_item}></span>
-                        <span className={s.burger_item}></span>
-                    </div>
-                    : <div className={s.header_menu_wrapper}>
+
+                {open
+                    ? <div className={s.burger_menu_wrapper}>
+                        <CloseOutlined className={s.close_icon} onClick={onMenuToggle}/>
                         {MENU_HEADER_LIST.map((item) => {
                             return (
-                                <Link href={`/${item.href}`} key={item.id} className={s.menu_link}>
+                                <Link href={`/${item.href}`} key={item.id} className={s.menu_link} onClick={onMenuToggle}>
                             <span>
                                 {item.title}
                             </span>
                                 </Link>
                             )
                         })}
-                    </div>}
+                    </div>
+                    : <>
+                        <Link href={`/invite`} className={s.menu_link}>
+                            <div className={s.header_logo}>А&П</div>
+                        </Link>
+                        {width < 768
+                            ? <div className={s.burger_menu} onClick={onMenuToggle}>
+                                <span className={s.burger_item}></span>
+                                <span className={s.burger_item}></span>
+                                <span className={s.burger_item}></span>
+                            </div>
+                            : <div className={s.header_menu_wrapper}>
+                                {MENU_HEADER_LIST.map((item) => {
+                                    return (
+                                        <Link href={`/${item.href}`} key={item.id} className={s.menu_link}>
+                            <span>
+                                {item.title}
+                            </span>
+                                        </Link>
+                                    )
+                                })}
+                            </div>}
+                    </>}
             </div>
         </section>
-    );
+    )
+        ;
 };
