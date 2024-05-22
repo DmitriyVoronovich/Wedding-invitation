47aecda18f3a0abd663581c10391d3b6b2c0d416
Dmitriy Vorornovich <dmitriy.vr20@gmail.com>
2024-05-22 22:36:28 +0300

    code refact

--- app/components/second/interrogation-content/interrogation-form/index.tsx
+++ app/components/second/interrogation-content/interrogation-form/index.tsx
@@ -7,9 +7,14 @@ import s from "./interrogation-form.module.scss";
 import './interrogation-form.css'
 
 export const InterrogationForm = () => {
-    const [moreThenOne, setMoreThenOne] = useState(false)
-    const [formLayout, setFormLayout] = useState('');
-    const [layout, setLayout] = useState('');
+    const [moreThenOne, setMoreThenOne] = useState(true)
+    const [formLayout, setFormLayout] = useState('yes');
+    const [layout, setLayout] = useState('yes');
+    const [selectedDrink, setSelectedDrink] = useState([]);
+
+    const onDrinkChange = (value: any) => {
+        setSelectedDrink(value);
+    }
 
     const [form] = Form.useForm();
 
@@ -28,8 +33,12 @@ export const InterrogationForm = () => {
         setLayout(e.target.value);
     };
 
-    const onFinish: FormProps<any>['onFinishFailed'] = (value: any) => {
+    const onFinish: FormProps['onFinish'] = (value: any) => {
         console.log('Success:', value);
+    };
+
+    const handleReset = () => {
+        form.resetFields();
     };
 
     return (
@@ -37,81 +46,96 @@ export const InterrogationForm = () => {
             <div className={s.container}>
                 <h2 className={s.section_title}>Ответьте на несколько вопросов</h2>
                 <Form form={form} className={s.section_form} onFinish={onFinish}>
-                    {moreThenOne ? <Form.Item name="presentGuests" className={s.form_item}>
+                    {moreThenOne
+                        ? <>
                             <h3 className={s.item_title}>Будете ли вы присутствовать на нашем торжестве?</h3>
-                            <Radio.Group className={s.item_radio_group}>
-                                <Radio.Button value="yes">Буду</Radio.Button>
-                                <Radio.Button value="no">Не буду</Radio.Button>
-                            </Radio.Group>
-                        </Form.Item> :
-                        <Form.Item name="presentGuests" className={s.form_item}>
-                            <h3 className={s.item_title}>Будете ли вы присутствовать на нашем торжестве?</h3>
-                            <div className={s.item_content_wrapper}>
-                                <Radio.Group className={s.item_radio_group} defaultValue={formLayout}
-                                             onChange={onFormLayoutChange}>
-                                    <Radio.Button value="yes">Буду</Radio.Button>
-                                    <Radio.Button value="no">Не буду</Radio.Button>
-                                    <Radio.Button value="any">Будем частично</Radio.Button>
+                            <Form.Item name="presentGuests" className={s.form_item}>
+                                <Radio.Group className={s.item_radio_group} value={'yes'}>
+                                    <Button value="yes">Буду</Button>
+                                    <Button value="no">Не буду</Button>
                                 </Radio.Group>
-                                {formLayout === 'any' && <Select
-                                    mode="multiple"
-                                    allowClear
-                                    className={s.item_selector}
-                                    placeholder="Пожалуйста, выберете кто будет присутствовать"
-                                    options={options}
-                                />}
-                            </div>
-                        </Form.Item>}
+                            </Form.Item>
+                        </>
+                         :
+                        <>
+                            <h3 className={s.item_title}>Будете ли вы присутствовать на нашем торжестве?</h3>
+                            <Form.Item name="presentGuests" className={s.form_item}>
+                                <div className={s.item_content_wrapper}>
+                                    <Radio.Group className={s.item_radio_group}
+                                                 onChange={onFormLayoutChange}
+                                                 value={formLayout}>
+                                        <Radio.Button value="yes">Буду</Radio.Button>
+                                        <Radio.Button value="no">Не буду</Radio.Button>
+                                        <Radio.Button value="any">Будем частично</Radio.Button>
+                                    </Radio.Group>
+                                    {formLayout === 'any' && <Select
+                                        mode="multiple"
+                                        allowClear
+                                        className={s.item_selector}
+                                        placeholder="Пожалуйста, выберете кто будет присутствовать"
+                                        options={options}
+                                    />}
+                                </div>
+                            </Form.Item>
+                        </>}
+                    <h3 className={s.item_title}>Будете ли вы присутствовать на венчании или же сразу отправитесь на
+                        банкет?</h3>
                     <Form.Item name="startPlace" className={s.form_item}>
-                        <h3 className={s.item_title}>Будете ли вы присутствовать на венчании или же сразу отправитесь на
-                            банкет?</h3>
-                        <Radio.Group className={s.item_radio_group}>
+                        <Radio.Group className={s.item_radio_group} value={'chirch'}>
                             <Radio.Button value="chirch">Будeм присутствовать на венчании</Radio.Button>
                             <Radio.Button value="inline">Сразу отправимся на банкет</Radio.Button>
                         </Radio.Group>
                     </Form.Item>
+                    <h3 className={s.item_title}>Как Вы планируете добираться?</h3>
                     <Form.Item name="isPrivateTransport" className={s.form_item}>
-                        <h3 className={s.item_title}>Как Вы планируете добираться?</h3>
                         <Radio.Group className={s.item_radio_group}>
                             <Radio.Button value="horizontal">Транспортом молодых на венчание,
                                 а затем на банкет</Radio.Button>
                             <Radio.Button value="inline">Личным транспортом</Radio.Button>
                         </Radio.Group>
                     </Form.Item>
-                    {moreThenOne ? <Form.Item name="presentOnSecondDay" className={s.form_item}>
+                    {moreThenOne
+                        ? <>
                             <h3 className={s.item_title}>Будете ли Вы на втором дне нашего торжества?</h3>
-                            <Radio.Group className={s.item_radio_group}>
-                                <Radio.Button value="yes">Буду</Radio.Button>
-                                <Radio.Button value="no">Не буду</Radio.Button>
-                            </Radio.Group>
-                        </Form.Item> :
-                        <Form.Item name="presentOnSecondDay" className={s.form_item}>
-                            <h3 className={s.item_title}>Будете ли Вы на втором дне нашего торжества?</h3>
-                            <div className={s.item_content_wrapper}>
-                                <Radio.Group className={s.item_radio_group} defaultValue={layout}
-                                             onChange={onLayoutChange}>
+                            <Form.Item name="presentOnSecondDay" className={s.form_item}>
+                                <Radio.Group className={s.item_radio_group}>
                                     <Radio.Button value="yes">Буду</Radio.Button>
                                     <Radio.Button value="no">Не буду</Radio.Button>
-                                    <Radio.Button value="any">Будем частично</Radio.Button>
                                 </Radio.Group>
-                                {layout === 'any' && <Select
-                                    mode="multiple"
-                                    allowClear
-                                    className={s.item_selector}
-                                    placeholder="Пожалуйста, выберете кто будет присутствовать"
-                                    options={options}
-                                />}
-                            </div>
-                        </Form.Item>}
+                            </Form.Item>
+                        </>
+                         :
+                        <>
+                            <h3 className={s.item_title}>Будете ли Вы на втором дне нашего торжества?</h3>
+                            <Form.Item name="presentOnSecondDay" className={s.form_item}>
+                                <div className={s.item_content_wrapper}>
+                                    <Radio.Group className={s.item_radio_group}
+                                                 onChange={onLayoutChange}
+                                                 value={layout}>
+                                        <Radio.Button value="yes">Буду</Radio.Button>
+                                        <Radio.Button value="no">Не буду</Radio.Button>
+                                        <Radio.Button value="any">Будем частично</Radio.Button>
+                                    </Radio.Group>
+                                    {layout === 'any' && <Select
+                                        mode="multiple"
+                                        allowClear
+                                        className={s.item_selector}
+                                        placeholder="Пожалуйста, выберете кто будет присутствовать"
+                                        options={options}
+                                    />}
+                                </div>
+                            </Form.Item>
+                        </>
+                    }
+                    <h3 className={s.item_title}>Нужен ли ночлег?</h3>
                     <Form.Item name="needSleepPlace" className={s.form_item}>
-                        <h3 className={s.item_title}>Нужен ли ночлег?</h3>
                         <Radio.Group className={s.item_radio_group}>
                             <Radio.Button value="yes">Нужен</Radio.Button>
                             <Radio.Button value="no">Не нужен</Radio.Button>
                         </Radio.Group>
                     </Form.Item>
+                    <h3 className={s.item_title}>Какие напитки Вы предпочитаете?</h3>
                     <Form.Item name="likeDrinks" className={s.form_item}>
-                        <h3 className={s.item_title}>Какие напитки Вы предпочитаете?</h3>
                         <Select
                             mode="multiple"
                             allowClear
@@ -119,6 +143,8 @@ export const InterrogationForm = () => {
                             style={{width: '450px'}}
                             placeholder="Пожалуйста выберете напитки"
                             options={alcoholicDrinks}
+                            value={selectedDrink}
+                            onChange={onDrinkChange}
                         />
                     </Form.Item>
                     <div className={s.button_wrapper}>
@@ -126,7 +152,7 @@ export const InterrogationForm = () => {
                             <Button className={s.description_button} htmlType="submit">Сохранить результат</Button>
                         </Form.Item>
                         <Form.Item>
-                            <Button className={s.description_button} htmlType="reset">Стереть результат</Button>
+                            <Button className={s.description_button} onClick={handleReset}>Стереть результат</Button>
                         </Form.Item>
                     </div>
                 </Form>
