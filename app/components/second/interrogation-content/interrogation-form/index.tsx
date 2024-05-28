724cc742f5b7c803c69a953f32720e0cd7fc49ad
Pavel_Varanovich <pavel_varanovich@epam.com>
2024-05-28 23:39:46 +0200

    WI-15 [FE] Fix issue

--- app/components/second/interrogation-content/interrogation-form/index.tsx
+++ app/components/second/interrogation-content/interrogation-form/index.tsx
@@ -100,13 +100,13 @@ export const InterrogationForm = ({inviteInfo, inviteId, onRespForm, singleGuest
 
     const presentInitialValue = (inviteInfo: any, paramName: string) => {
         let resultValue;
-        if (!inviteInfo.surveyResponses[paramName]) {
+        if (!inviteInfo.surveyResponses?.[paramName]) {
             return ''
         }
-        if (!inviteInfo.surveyResponses[paramName].length) {
+        if (!inviteInfo.surveyResponses?.[paramName].length) {
             resultValue = 'no';
         } else {
-            resultValue = inviteInfo.surveyResponses[paramName].length === inviteInfo.guests.length ? 'yes' : 'any';
+            resultValue = inviteInfo.surveyResponses?.[paramName].length === inviteInfo.guests.length ? 'yes' : 'any';
         }
 
         return resultValue
