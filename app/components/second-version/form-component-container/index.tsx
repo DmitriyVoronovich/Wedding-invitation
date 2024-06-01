5e6ced7e52fe6ed5ae4cf949dc03dc13bb66ccba
Dmitriy Vorornovich <dmitriy.vr20@gmail.com>
2024-06-01 10:02:18 +0300

    code refactor

--- app/components/second-version/form-component-container/index.tsx
+++ app/components/second-version/form-component-container/index.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import {Fade} from "react-awesome-reveal";
 // @ts-ignore
 import useSound from "use-sound";
